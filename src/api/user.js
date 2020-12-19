7e6874f430030fb5005937111f457938cd3f2f14
yelin <you@example.com>
2020-12-19 16:27:09 +0800

    优化代码

--- src/api/user.js
+++ src/api/user.js
@@ -1,41 +1,18 @@
 import request from '@/utils/request'
 
-// export function login(data) {
-//   return request({
-//     url: '/vue-admin-template/user/login',
-//     method: 'post',
-//     data
-//   })
-// }
-
-// export function getInfo(token) {
-//   return request({
-//     url: '/vue-admin-template/user/info',
-//     method: 'get',
-//     params: { token }
-//   })
-// }
-
-// export function logout() {
-//   return request({
-//     url: '/vue-admin-template/user/logout',
-//     method: 'post'
-//   })
-// }
-
 export function getInfo(token) {
   return request({
-    url: "/users/info",
-    method: "get",
-    params: { token } //url序列化参数
-  });
+    url: '/users/info',
+    method: 'get',
+    params: { token } // url序列化参数
+  })
 }
 
 export function logout() {
   return request({
-    url: "/users/logout",
-    method: "get"
-  });
+    url: '/users/logout',
+    method: 'get'
+  })
 }
 
 export function login(data) {
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -53,26 +53,11 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
 import md5 from 'crypto-js/md5'
 
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
     return {
       loginForm: {
         username: 'admin',
